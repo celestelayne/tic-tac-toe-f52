d031d5c8847e77bb258e03eb3ab59992bf01ba63
Celeste Layne <celeste.layne@generalassemb.ly>
2019-08-13 00:23:00 -0400

    run linter

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 console.log('this works!');
 
 // 0 | 1 | 2
-//---+---+---
+// ---+---+---
 // 3 | 4 | 5
-//---+---+---
+// ---+---+---
 // 6 | 7 | 8
 
 // global variables
@@ -30,15 +30,15 @@ const winStates = [
   [2, 4, 6],
   [0, 3, 6],
   [1, 4, 7],
-  [2, 5, 8]
+  [2, 5, 8],
 ];
 
 // functions
 
 const resetBoard = () => {
-  console.log('clicked')
+  // console.log('clicked')
   // remove all the Xs & Os from the squares
-  for (i in squares) {
+  for (let i in squares) {
     squares[i].textContent = ''
   }
 
@@ -47,18 +47,17 @@ const resetBoard = () => {
   playerO = [];
 
   // reset winning player in aside
-  player.textContent = ''
+  player.textContent = '';
 
   // set initial turn back to 'X'
   turn = 'X';
-}
+};
 
 const setSquare = (square) => {
-
   // create svg element and append to each square
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("width", "98");
-  svg.setAttribute("height", "98");
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', '98');
+  svg.setAttribute('height', '98');
   square.appendChild(svg);
 
   // create a circle and set attributes
@@ -79,36 +78,34 @@ const setSquare = (square) => {
   line.setAttribute("stroke", "#2D4558");
   line.setAttribute("stroke-width", "3");
 
-  console.log(turn)
+  // console.log(turn);
 
   square.innerText = turn;
   turn === 'X' ? turn = 'O' : turn = 'X';
-
-}
+};
 
 const addPiece = (e) => {
   // Prevents the event from bubbling up to the board,
   // but does not prevent clicking on other squares,
   // so the event is only registered by the element clicked.
-  e.stopPropagation()
+  e.stopPropagation();
 
-  let currentSquare = e.target;
+  const currentSquare = e.target;
 
-  if (currentSquare.className === 'square'){
+  if (currentSquare.className === 'square') {
     // pass the current square to the setSquare function
-    setSquare(currentSquare)
+    setSquare(currentSquare);
   }
 
   // pass the current square to the game logic
   playGame(currentSquare)
-}
+};
 
 const playGame = (val) => {
-
   // grab 'data-id' attribute for each square
-  let dataId = val.getAttribute('data-id')
+  let dataId = val.getAttribute('data-id');
 
-  if (val.textContent === 'X'){
+  if (val.textContent === 'X') {
 
     playerX.push(parseInt(dataId))
     // sort the player moves for future compare function
@@ -125,47 +122,46 @@ const playGame = (val) => {
 }
 
 const compare = (arr, playerx, playero) => {
-  let player1, player2;
+  let player1 = `Player X`;
+  let player2 = `Player O`;
 
-  if(playerx.length <= 3 ) {
-    player1 = `Player X`
-    let wins = arr.filter(combo => combo.filter(el => {
+  if (playerx.length <= 3) {
+    // player1 = `Player X`
+    const wins = arr.filter((combo) => combo.filter((el) => {
       return playerx.indexOf(el) > -1;
-    }).length == 3);
+    }).length === 3);
     // console.log(wins, playerx)
     checkWins(wins, playerx, player1)
   }
 
-  if(playero.length <= 3 ) {
-    player2 = `Player O`
-    let wins = arr.filter(combo => combo.filter(el => {
+  if (playero.length <= 3) {
+    // player2 = `Player O`
+    const wins = arr.filter((combo) => combo.filter((el) => {
       // return the first index at which a given element can be found
       // in the array, or -1 if it is not present.
       return playero.indexOf(el) > -1;
-    }).length == 3);
+    }).length === 3);
 
     checkWins(wins, playero, player2)
   }
 
   // if there is a draw
-  if(playerx.length >= 5 || playero.length >= 5) {
+  if (playerx.length >= 5 || playero.length >= 5) {
     setTimeout(() => alert(`we have a draw`), 250);
-    return;
+    // return;
   }
-}
+};
 
 
 const checkWins = (a, b, c) => {
-
-    if(JSON.stringify(a[0]) === JSON.stringify(b)){
-      // setTimeout(() => alert(`${c} wins`), 250);
-      player.textContent = `${c}`
-      return false;
-    }
-
-}
+  if (JSON.stringify(a[0]) === JSON.stringify(b)) {
+    // setTimeout(() => alert(`${c} wins`), 250);
+    player.textContent = `${c}`
+    return false;
+  }
+};
 
 // event handlers
 
-newGameBtn.addEventListener('click', resetBoard, false)
-board.addEventListener('click', addPiece)
+newGameBtn.addEventListener('click', resetBoard);
+board.addEventListener('click', addPiece);
